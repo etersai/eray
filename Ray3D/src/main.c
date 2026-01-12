c00bebd49a357699c51f60da89bf1b50346ee70d
ethersai <eter_wheyy@proton.me>
2026-01-12 22:28:53 +0100

    Quick commit on 2026-01-12 22:28:53

--- Ray3D/src/main.c
+++ Ray3D/src/main.c
@@ -1,21 +1,16 @@
 #include "../include/raylib.h"
 #include "../include/raymath.h"
 
-#define WORLD_SIZE 8   // Size of our voxel world (8x8x8 cubes)
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
     InitWindow(screenWidth, screenHeight, "raylib [models] example - basic voxel");
 
     DisableCursor();                    // Lock mouse to window center
+    
+
+                                        
 
     // Define the camera to look into our 3d world (first person)
     Camera3D camera = { 0 };
@@ -26,22 +21,11 @@ int main(void)
     camera.projection = CAMERA_PERSPECTIVE;              // Camera projection type
 
     // Create a cube model
-    Mesh cubeMesh = GenMeshCube(1.0f, 1.0f, 1.0f);      // Create a unit cube mesh
-    Model cubeModel = LoadModelFromMesh(cubeMesh);       // Convert mesh to a model
-    cubeModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].color = BEIGE;
-
-    // Initialize voxel world - fill with voxels
-    bool voxels[WORLD_SIZE][WORLD_SIZE][WORLD_SIZE] = { false };
-    for (int x = 0; x < WORLD_SIZE; x++)
-    {
-        for (int y = 0; y < WORLD_SIZE; y++)
-        {
-            for (int z = 0; z < WORLD_SIZE; z++)
-            {
-                voxels[x][y][z] = true;
-            }
-        }
-    }
+    // Mesh cubeMesh = GenMeshCube(1.0f, 1.0f, 1.0f);      // Create a unit cube mesh
+    // Model cubeModel = LoadModelFromMesh(cubeMesh);       // Convert mesh to a model
+    // cubeModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].color = BEIGE;
+    
+    Model my_model = LoadModel("models/bizzare.gltf");
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
@@ -49,49 +33,8 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+        UpdateCamera(&camera, CAMERA_FREE);
 
-        // Handle voxel removal with mouse click
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        {
-            // Cast a ray from the screen center (where crosshair would be)
-            Vector2 screenCenter = { screenWidth/2.0f, screenHeight/2.0f };
-            Ray ray = GetMouseRay(screenCenter, camera);
-
-            // Check ray collision with all voxels
-            bool voxelRemoved = false;
-            for (int x = 0; (x < WORLD_SIZE) && !voxelRemoved; x++)
-            {
-                for (int y = 0; (y < WORLD_SIZE) && !voxelRemoved; y++)
-                {
-                    for (int z = 0; (z < WORLD_SIZE) && !voxelRemoved; z++)
-                    {
-                        if (!voxels[x][y][z]) continue; // Skip empty voxels
-
-                        // Build a bounding box for this voxel
-                        Vector3 position = { (float)x, (float)y, (float)z };
-                        BoundingBox box = {
-                            (Vector3){ position.x - 0.5f, position.y - 0.5f, position.z - 0.5f },
-                            (Vector3){ position.x + 0.5f, position.y + 0.5f, position.z + 0.5f }
-                        };
-
-                        // Check ray-box collision
-                        RayCollision collision = GetRayCollisionBox(ray, box);
-                        if (collision.hit)
-                        {
-                            voxels[x][y][z] = false;    // Remove this voxel
-                            voxelRemoved = true;        // Exit all loops
-                        }
-                    }
-                }
-            }
-        }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -99,27 +42,10 @@ int main(void)
             BeginMode3D(camera);
 
                 DrawGrid(10, 1.0f);
-
-                // Draw all voxels
-                for (int x = 0; x < WORLD_SIZE; x++)
-                {
-                    for (int y = 0; y < WORLD_SIZE; y++)
-                    {
-                        for (int z = 0; z < WORLD_SIZE; z++)
-                        {
-                            if (!voxels[x][y][z]) continue;
-
-                            Vector3 position = { (float)x, (float)y, (float)z };
-                            DrawModel(cubeModel, position, 1.0f, BEIGE);
-                            DrawCubeWires(position, 1.0f, 1.0f, 1.0f, BLACK);
-                        }
-                    }
-                }
+                DrawModel(my_model, (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
 
             EndMode3D();
 
-            DrawText("Left-click a voxel to remove it!", 10, 10, 20, DARKGRAY);
-            DrawText("WASD to move, mouse to look around", 10, 35, 10, GRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -127,7 +53,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadModel(cubeModel);
+    UnloadModel(my_model);
     CloseWindow();
     //--------------------------------------------------------------------------------------
 
