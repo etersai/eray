8abb1bc7aa5034841ec696f027e7569ffc52370c
ethersai <eter_wheyy@proton.me>
2026-01-18 12:42:40 +0100

    Quick commit on 2026-01-18 12:42:40

--- softsdl/scratch.c
+++ softsdl/scratch.c
@@ -1,56 +1,24 @@
-//  // ROTATE CUBE.
-    // float rot = PI;
-    // for (size_t i = 0; i < ARRLEN(cube); i+=3) {
-    //     float x = cube[i];
-    //     float z = cube[i+2];
-    //     cube[i]   = cos(rot) * x - sin(rot) * z;
-    //     cube[i+2] = sin(rot) * x + cos(rot) * z;
-    // }
-
-    // TRANSLATE CUBE.
-  //  for (size_t i = 0; i < ARRLEN(cube); i+=3) {
-   //     cube[i+2] += -4.0f;
-   // }
- //
-    // int points[16] = {0};
-    // int pt = 0;
-    // for (size_t vertex = 0; vertex < ARRLEN(cube); vertex+=3) {
-    //     if (cube[vertex+2] == 0.0f) continue; // clipping XD
-    //     float x_proj = (cube[vertex] / -cube[vertex+2]) / aspect_ratio;
-    //     float y_proj = cube[vertex+1] / -cube[vertex+2];
-    //     float x_proj_remap = (x_proj + 1) / 2;
-    //     float y_proj_remap = (y_proj + 1) / 2;
-    //     int x_proj_pix = (int)(x_proj_remap * ec.width);
-    //     int y_proj_pix = (int)(y_proj_remap * ec.height);
-    //     points[pt] = x_proj_pix;
-    //     points[pt+1] = y_proj_pix;
-    //     printf("[x:%f, y:%f, z:%f]\n",cube[vertex], cube[vertex+1], cube[vertex+2]);
-    //     pt+=2;
-    // }
-    //
-    // for (size_t i = 0; i < ARRLEN(points);) {
-    //     printf("[%d, %d]\n", points[i], points[i+1]);
-    //     i+=2;
-    // }
-    // fflush(stdout);
-    //
-    // for (size_t i = 0; i < ARRLEN(cube_idx);) {
-    //     int idx1 = cube_idx[i] * 2;
-    //     int idx2 = cube_idx[i+1] * 2;
-    //     int x1 = points[idx1];
-    //     int y1 = points[idx1+1];
-    //     int x2 = points[idx2];
-    //     int y2 = points[idx2+1];
-    //     eray_plot_line(ec, x1, y1, x2, y2, ERAY_COLOR_RED);
-    //     i+=2;
-    // }   
-    //
-    //
-
 #include <stdio.h>
 
 typedef struct { float x; float y; float z; } vecf3;
 typedef struct { float data[4][4]; } matf4x4;
+
+#define MATRIX4X4_PRINT_FORMAT "[%.2f, %.2f, %.2f, %.2f]\n"
+#define VECF3_PRINT_FORMAT "[%.2f, %.2f, %.2f]\n"
+static void matf4x4_print(const matf4x4* matrix)
+{
+    fprintf(stdout, MATRIX4X4_PRINT_FORMAT, matrix->data[0][0], matrix->data[0][1], matrix->data[0][2], matrix->data[0][3]);
+    fprintf(stdout, MATRIX4X4_PRINT_FORMAT, matrix->data[1][0], matrix->data[1][1], matrix->data[1][2], matrix->data[1][3]);
+    fprintf(stdout, MATRIX4X4_PRINT_FORMAT, matrix->data[2][0], matrix->data[2][1], matrix->data[2][2], matrix->data[2][3]);
+    fprintf(stdout, MATRIX4X4_PRINT_FORMAT, matrix->data[3][0], matrix->data[3][1], matrix->data[3][2], matrix->data[3][3]);
+    fflush(stdout);
+}
+
+static void vecf3_print(vecf3 vec)
+{
+    fprintf(stdout, VECF3_PRINT_FORMAT, vec.x, vec.y, vec.z);
+    fflush(stdout);
+}
 
 static inline void matf4x4_identity(matf4x4* matrix)
 {
@@ -75,6 +43,117 @@ static inline void matf4x4_identity(matf4x4* matrix)
     matrix->data[3][3] = 1.0f;
 }
 
+static inline void matf4x4_test(matf4x4* matrix)
+{
+    matrix->data[0][0] = 1.0f;
+    matrix->data[0][1] = 2.0f;
+    matrix->data[0][2] = 3.0f;
+    matrix->data[0][3] = 4.0f;
+    
+    matrix->data[1][0] = 5.0f;
+    matrix->data[1][1] = 6.0f;
+    matrix->data[1][2] = 7.0f;
+    matrix->data[1][3] = 8.0f; 
+
+    matrix->data[2][0] = 9.0f;
+    matrix->data[2][1] = 10.0f;
+    matrix->data[2][2] = 11.0f;
+    matrix->data[2][3] = 12.0f;
+    
+    matrix->data[3][0] = 13.0f;
+    matrix->data[3][1] = 14.0f;
+    matrix->data[3][2] = 15.0f;
+    matrix->data[3][3] = 16.0f;
+}
+
+static inline void matf4x4_translate(matf4x4* matrix, float x, float y, float z)
+{
+    matrix->data[3][0] += x; 
+    matrix->data[3][1] += y; 
+    matrix->data[3][2] += z; 
+}
+
+static inline void matf4x4_translate_set(matf4x4* matrix, float x, float y, float z)
+{
+    matrix->data[3][0] = x; 
+    matrix->data[3][1] = y; 
+    matrix->data[3][2] = z; 
+}
+
+static inline void matf4x4_scale_uniform(matf4x4* matrix, float scale)
+{
+    matrix->data[0][0] *= scale;
+    matrix->data[1][1] *= scale;
+    matrix->data[2][2] *= scale;
+}
+
+static inline void matf4x4_scale(matf4x4* matrix, float sx, float sy, float sz)
+{
+    matrix->data[0][0] *= sx;
+    matrix->data[1][1] *= sy;
+    matrix->data[2][2] *= sz;
+}
+
+static inline void matf4x4_transpose_to_new(const matf4x4* matrix, matf4x4* result)
+{
+    // first row to column
+    result->data[0][0] = matrix->data[0][0];
+    result->data[1][0] = matrix->data[0][1];
+    result->data[2][0] = matrix->data[0][2];
+    result->data[3][0] = matrix->data[0][3];
+    
+    // second row to second column
+    result->data[0][1] = matrix->data[1][0];
+    result->data[1][1] = matrix->data[1][1];
+    result->data[2][1] = matrix->data[1][2];
+    result->data[3][1] = matrix->data[1][3];
+
+    // third row to third column
+    result->data[0][2] = matrix->data[2][0];
+    result->data[1][2] = matrix->data[2][1];
+    result->data[2][2] = matrix->data[2][2];
+    result->data[3][2] = matrix->data[2][3];
+
+    // fourth row to fourth column
+    result->data[0][3] = matrix->data[3][0];
+    result->data[1][3] = matrix->data[3][1];
+    result->data[2][3] = matrix->data[3][2];
+    result->data[3][3] = matrix->data[3][3];
+}
+
+
+static inline void matf4x4_transpose_in_place(matf4x4* matrix)
+{
+    matf4x4 temp;
+    
+    // first row to column
+    temp.data[0][0] = matrix->data[0][0];
+    temp.data[1][0] = matrix->data[0][1];
+    temp.data[2][0] = matrix->data[0][2];
+    temp.data[3][0] = matrix->data[0][3];
+    
+    // second row to second column
+    temp.data[0][1] = matrix->data[1][0];
+    temp.data[1][1] = matrix->data[1][1];
+    temp.data[2][1] = matrix->data[1][2];
+    temp.data[3][1] = matrix->data[1][3];
+
+    // third row to third column
+    temp.data[0][2] = matrix->data[2][0];
+    temp.data[1][2] = matrix->data[2][1];
+    temp.data[2][2] = matrix->data[2][2];
+    temp.data[3][2] = matrix->data[2][3];
+
+    // fourth row to fourth column
+    temp.data[0][3] = matrix->data[3][0];
+    temp.data[1][3] = matrix->data[3][1];
+    temp.data[2][3] = matrix->data[3][2];
+    temp.data[3][3] = matrix->data[3][3];
+
+    *matrix = temp;
+}
+
+
 static inline vecf3 transformPoint(const vecf3* point, const matf4x4* matrix)
 {
     float x = point->x * matrix->data[0][0] + point->y * matrix->data[1][0] + 
@@ -88,10 +167,15 @@ static inline vecf3 transformPoint(const vecf3* point, const matf4x4* matrix)
 
 int main(void)
 {
-    vecf3 point = {-0.5f, 0.5f, 0.5f};
-    matf4x4 rotation = {0.0f};
-    matf4x4_identity(&rotation);
-    vecf3 trans = transformPoint(&point, &rotation); 
-    printf("%f, %f, %f", trans.x, trans.y, trans.z);
+    vecf3 point = {0.0f};
+    matf4x4 local_to_world;
+    matf4x4_identity(&local_to_world);
+    matf4x4_translate(&local_to_world, 2.0f, 3.0f, -3.0f);
+
+    matf4x4_print(&local_to_world);
+    putchar('\n');
+
+    vecf3 new_point = transformPoint(&point, &local_to_world);
+    vecf3_print(new_point);
     return 0;
 }
