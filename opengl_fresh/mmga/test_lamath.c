6f92713e96fb3e7461302bcdfb185ca24c12573d
ethersai <eter_wheyy@proton.me>
2026-01-27 15:48:22 +0100

    Quick commit on 2026-01-27 15:48:22

--- opengl_fresh/mmga/test_lamath.c
+++ opengl_fresh/mmga/test_lamath.c
@@ -6,7 +6,7 @@ int main(void)
     matf4x4 m = matf4x4_I_give();
 
     m.col1 = (vecf4){1.0f, 2.0f, -1.0f, 2.0f};
-    m.col2 = (vecf4){0.0f, 5.0f,  2.0f, 1.0f};
+    m.col2 = (vecf4){10.0f, 5.0f,  2.0f, 1.0f};
     m.col3 = (vecf4){4.0f, 0.0f,  3.0f, -2.0f};
     m.col4 = (vecf4){-6.0f, 3.0f, 5.0f, 3.0f};
 
