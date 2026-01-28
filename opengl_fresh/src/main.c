5437cafde0a31270eccb7468ae1fdc42737f18d0
ethersai <eter_wheyy@proton.me>
2026-01-28 17:25:22 +0100

    Quick commit on 2026-01-28 17:25:22

--- opengl_fresh/src/main.c
+++ opengl_fresh/src/main.c
@@ -167,12 +167,30 @@ int main(void)
         log_print_prefix("asset_load_error", "failed to load '%s'\n", asset_path);
         abort();
     }
+   // texture_grass.height
+    log_print_n_flush("HEHE HERE: %d\n", texture_grass.height);
     
     // cubemap stuff.
     GLuint cubemap_texture;
     glGenTextures(1, &cubemap_texture);
     glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap_texture);
 
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE); 
+
+    // int width, height, nrChannels;
+    // unsigned char *data;  
+    // for(unsigned int i = 0; i < textures_faces.size(); i++)
+    // {
+    //     data = stbi_load(textures_faces[i].c_str(), &width, &height, &nrChannels, 0);
+    //     glTexImage2D(
+    //         GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 
+    //         0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+    //     );
+    // }
 
 
 
@@ -450,6 +468,10 @@ Texture texture_create_from_memory(unsigned char* data, int width, int height, i
     // Generate gl texutre with picked options.
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);       
+
+    texture.width = width;
+    texture.height = height;
+    texture.num_color_channels = color_channels;
 
     return texture;
 }
