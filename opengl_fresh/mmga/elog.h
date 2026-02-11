1a383481fa0b369fed8bae5ca7dd26eeefe913bd
ethersai <eter_wheyy@proton.me>
2026-02-11 20:50:34 +0100

    Quick commit on 2026-02-11 20:50:34

---
+++ opengl_fresh/mmga/elog.h
@@ -0,0 +1,87 @@
+#ifndef ELOG_H_
+#define ELOG_H_
+
+// elog_ aka C victorinox.
+// Sometimes i just want to quickly print some value.
+// Basically save yourself some typing wrapper.
+// fflush intentional.
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifndef ELOGDEF
+#define ELOGDEF static inline
+#endif /* ELOGDEF */
+#ifndef ELOG_TARGET
+#define ELOG_TARGET stderr
+#endif /* ELOG_TARGET */
+#ifndef ELOG_TAG
+#define ELOG_TAG "[elog]: "
+#endif /* ELOG_TAG */
+
+ELOGDEF void elog_zu(size_t val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%zu\n", val);
+    fflush(ELOG_TARGET);
+}
+ELOGDEF void elog_d(int val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%d\n", val);
+    fflush(ELOG_TARGET);
+}
+ELOGDEF void elog_f(float val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%f\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_s(const char* str)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%s\n", str);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_u(unsigned int val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%u\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_llu(unsigned long long val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%llu\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_lld(long long val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%lld\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_p(const void* p)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%p\n", p);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_lu(unsigned long val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%lu\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_ld(long val)
+{
+    fprintf(ELOG_TARGET, ELOG_TAG "%ld\n", val);
+    fflush(ELOG_TARGET);
+}
+
+ELOGDEF void elog_abort(const char* str)
+{
+    elog_s(str);
+    abort();
+}
+
+#endif /* ELOG_H_ */
--- opengl_fresh/mmga/string8.c
+++ opengl_fresh/mmga/string8.c
@@ -1,9 +1,11 @@
-#ifndef ERAY_COMMON_H_
-#define ERAY_COMMON_H_
-
+#include "elog.h"
 #include <stdint.h>
 
-// BASE TYPES
+typedef struct {
+
+} string8;
+
+// base types. credit RadDebugger.
 typedef uint8_t  U8;
 typedef uint16_t U16;
 typedef uint32_t U32;
@@ -15,11 +17,8 @@ typedef int64_t  S64;
 typedef float    F32;
 typedef double   F64;
 
-typedef struct {
-    F32 r;
-    F32 g;
-    F32 b;
-    F32 a;
-} Colorek;
-
-#endif /* ERAY_COMMON_H_ */
+int main(void)
+{
+    elog_s("HELLO WORDLS");
+    return 0;
+}
