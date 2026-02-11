2558b2f18ed148cae246562ec9408f1fe5390b20
ethersai <eter_wheyy@proton.me>
2026-02-11 21:23:06 +0100

    Quick commit on 2026-02-11 21:23:06

--- opengl_fresh/mmga/string8.c
+++ opengl_fresh/mmga/string8.c
@@ -1,10 +1,6 @@
 #include "elog.h"
 #include <stdint.h>
 
-typedef struct {
-
-} string8;
-
 // base types. credit RadDebugger.
 typedef uint8_t  U8;
 typedef uint16_t U16;
@@ -16,6 +12,11 @@ typedef int32_t  S32;
 typedef int64_t  S64;
 typedef float    F32;
 typedef double   F64;
+
+typedef struct {
+    U8 *str;    
+    U64 size;
+} string8;
 
 int main(void)
 {
