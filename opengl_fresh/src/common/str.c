d121093ec485d1b093c92ba1cd426afc6d1ce674
ethersai <eter_wheyy@proton.me>
2026-02-13 23:57:40 +0100

    Quick commit on 2026-02-13 23:57:40

--- opengl_fresh/src/common/str.c
+++ opengl_fresh/src/common/str.c
@@ -11,21 +11,17 @@ typedef struct {
     u64 size;
 } string8;
 
-internal string8 str8(u8* str, u64 size)
+internal inline string8 str8(u8* str, u64 size)
 {
     return (string8){str,size};
 }
 
-internal void str8_print(string8 str)
+internal inline void str8_print(string8 str)
 {
     fwrite(str.bytes, 1, str.size, stderr);
 }
 
 #define str8_lit(str) str8((u8*)(str), sizeof(str) - 1)
-
-// log_print_n_flush("[FPS: %d | DELTA TIME: %f]\n", frames, delta_time);
-// log_print_n_flush("[CAMERA POS] X: %f, Y: %f, Z: %f\n", program_ctx.camera.pos.x, program_ctx.camera.pos.y, program_ctx.camera.pos.z);
-// log_print_n_flush("[CAMERA DIR] X: %f, Y: %f, Z: %f\n", program_ctx.camera.orientation.x, program_ctx.camera.orientation.y, program_ctx.camera.orientation.z);
 
 internal string8 str8_format(Arenka* arena, char* fmt, ...)
 {
@@ -40,12 +36,14 @@ internal string8 str8_format(Arenka* arena, char* fmt, ...)
     return (string8){bytes, num_chars_wo_null_byte};
 }
 
-global Arenka string_arena;
-
+global Arenka scratch_arena;
 int main(void)
 {
     elog_s("PROGRAM INIT...");
-    string_arena = arenka_map(MB(10));
-    arenka_unmap(&string_arena);
+    scratch_arena = arenka_map(MB(10000));
+    if (scratch_arena.addr_start == NULL) {
+        elog_abort("memory mapping failed");
+    }
+    arenka_unmap(&scratch_arena);
     return 0;
 }
